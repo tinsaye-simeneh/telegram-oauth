9089d20c4a4a1aa2d7f9e1c6b6ea839c2d4c6229
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-04-19 10:28:06 +0300

    initial test files created

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Script from "next/script";
+import { TLoginButton, TLoginButtonSize } from "react-telegram-auth";
+
+export default function Home() {
+  return (
+    <TLoginButton
+      botName="TGoauthbot"
+      buttonSize={TLoginButtonSize.Medium}
+      lang="en"
+      usePic={false}
+      cornerRadius={20}
+      onAuthCallback={(user) => {
+        console.log("Hello, user!", user);
+      }}
+      requestAccess={"write"}
+      additionalClasses={"css-class-for-wrapper"}
+    />
+  );
+}
