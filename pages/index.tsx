1e561a31e9f74e6488c9dbe8d28b9aa638622ffb
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-04-19 11:29:00 +0300

    Profile page removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,34 @@ import Profile from "./Profile";
 
 export default function Home() {
   const [user, setUser] = useState(null);
-  const firstName2 = "first_name";
+
   return (
-    <TLoginButton
-      botName="TGoauthbot"
-      buttonSize={TLoginButtonSize.Medium}
-      lang="en"
-      usePic={false}
-      cornerRadius={20}
-      onAuthCallback={(user) => {
-        console.log(user);
-        return <Profile firstName={firstName2} />;
-        window.location.href = "/Profile";
-      }}
-      requestAccess={"write"}
-    />
+    <div>
+      {user ? (
+        <Profile firstName={user.first_name} />
+      ) : (
+        <div>
+          <Head>
+            <title>Telegram Login</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <main>
+            <h1 className="title">Telegram Login</h1>
+            <TLoginButton
+              botName="TGoauthbot"
+              buttonSize={TLoginButtonSize.Medium}
+              lang="en"
+              usePic={false}
+              cornerRadius={20}
+              onAuthCallback={(user) => {
+                console.log(user);
+                setUser(user);
+              }}
+              requestAccess={"write"}
+            />
+          </main>
+        </div>
+      )}
+    </div>
   );
 }
